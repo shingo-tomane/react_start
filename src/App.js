2b6275faefd71ce513194929d861769c73ca6520
shingo-tomane <142208852+shingo-tomane@users.noreply.github.com>
2023-08-20 00:49:05 +0900

    追加

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import React, { useState } from "react";
 import ColorfullMessage from "./components/ColorfullMessage";
 
 const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
   const onClickButton = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
       <ColorfullMessage color="blue">お元気ですか？</ColorfullMessage>
       <ColorfullMessage color="pink">お元気です</ColorfullMessage>
       <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickSwitchShowFlag}>ON/OFF</button>
       <p>{num}</p>
+      {faceShowFlag && <p>( ﾟДﾟ)</p>}
     </>
   );
 };
